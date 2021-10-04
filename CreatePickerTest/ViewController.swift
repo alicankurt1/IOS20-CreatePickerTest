0ae79e04bbd50f2580383d5a4c5820ed95ea5e47
Alican Kurt <kurta15@itu.edu.tr>
2021-10-04 10:57:24 +0300

    Initial Commit

--- CreatePickerTest/ViewController.swift
+++ CreatePickerTest/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var categoryPicker: UIPickerView!
+    let data = ["Apple","Orange","Cherry"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        categoryPicker.delegate = self
+        categoryPicker.dataSource = self
+        
+        
+        
     }
 
+    @IBAction func buttonClicked(_ sender: Any) {
+        if categoryPicker.isHidden == false{
+            categoryPicker.isHidden = true
+        }else{
+            categoryPicker.isHidden = false
+        }
+        
+        
+    }
+    
+}
 
+
+
+
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[row]
+    }
+   
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
+        
+    }
+    
+   
+    
+    
 }
 
