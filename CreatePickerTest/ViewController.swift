2a4247a7b6d95ca1463a89a3906ad9a93034a601
Alican Kurt <kurta15@itu.edu.tr>
2021-10-04 10:44:35 +0300

    Initial Commit

---
+++ CreatePickerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreatePickerTest
+//
+//  Created by Alican Kurt on 4.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
